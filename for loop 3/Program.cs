bb4459b0150b4f7d252ee62c0c21f4933d67c299
Your Name <you@example.com>
2020-12-19 19:53:57 +0500

    first commit

---
+++ for loop 3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_loop_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <=10; i++)
+            {
+                if (i==5)
+                {
+                    continue;
+                }
+                if (i==6)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
